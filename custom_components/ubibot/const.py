57ec26abf7af9827b1f5fb0c67fd92446a08e8a6
Daniël Kluivingh <dkluivingh@gmail.com>
2024-11-23 22:33:26 +0100

    Update const.py

--- custom_components/ubibot/const.py
+++ custom_components/ubibot/const.py
@@ -1,37 +1,29 @@
 """Constants for Ubibot module."""
 from homeassistant.const import (
-    DEVICE_CLASS_HUMIDITY,
-    DEVICE_CLASS_ILLUMINANCE,
-    DEVICE_CLASS_TEMPERATURE,
-    DEVICE_CLASS_SIGNAL_STRENGTH,
-    TEMP_CELSIUS,
-    SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+    UnitOfTemperature,
+)
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
 )
 
 SENSOR_TYPES = {
     "temperature": {
-        "class": DEVICE_CLASS_TEMPERATURE,
-        "unit": TEMP_CELSIUS,
+        "class": SensorDeviceClass.TEMPERATURE,
+        "unit": UnitOfTemperature.CELSIUS,
         "icon": "mdi:thermometer",
         "field": "field1",
     },
     "humidity": {
-        "class": DEVICE_CLASS_HUMIDITY,
+        "class": SensorDeviceClass.HUMIDITY,
         "unit": "%",
         "icon": "mdi:water-percent",
         "field": "field2",
     },
     "lux": {
-        "class": DEVICE_CLASS_ILLUMINANCE,
+        "class": SensorDeviceClass.ILLUMINANCE,
         "unit": "lx",
         "icon": "mdi:lightbulb-on-outline",
         "field": "field3",
-    },
-    "wifi_rssi": {
-        "class": DEVICE_CLASS_SIGNAL_STRENGTH,
-        "unit": SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
-        "icon": "mdi:wifi",
-        "field": "field5",
     },
 }
 
